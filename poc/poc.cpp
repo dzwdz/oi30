53600852615a1306191041527285a164d9b4f824
dzwdz <kg67199@gmail.com>
2022-10-28 19:28:03 +0200

    poc: some automated tests

--- poc/poc.cpp
+++ poc/poc.cpp
@@ -16,6 +16,16 @@ static uint32_t highest[MAXAMT];
 
 static uint32_t fullLen, subLen, carKinds; /* <= MAXAMT */
 
+void dump() {
+	printf("%u %u %u\n", fullLen, subLen, carKinds);
+	for (uint32_t i = 0; i < fullLen; i++)
+		printf("%u ", full[i]);
+	puts("");
+	for (uint32_t i = 0; i < subLen; i++)
+		printf("%u ", sub[i]);
+	puts("");
+}
+
 void printarr(int32_t *arr, size_t len) {
 	for (size_t i = 0; i < len; i++) {
 		printf("%d ", arr[i]);
@@ -27,14 +37,11 @@ bool check_naive(uint32_t i) {
 	uint32_t cur = full[i];
 	uint32_t fromLeft = leftElems[i] + 1;
 	uint32_t fromRight = rightElems[i] + 1;
-	bool inLeft = false, inRight = false;
-	for (uint32_t j = 0; j < fromLeft; j++)
-		if (sub[j] == cur)
-			inLeft = true;
-	for (uint32_t j = 0; j < fromRight; j++)
-		if (sub[subLen - 1 - j] == cur)
-			inRight = true;
-	return inLeft && inRight;
+	assert(fromRight <= subLen);
+	for (uint32_t i = subLen - fromRight; i < fromLeft; i++)
+		if (sub[i] == cur)
+			return true;
+	return false;
 }
 
 bool check_highest(uint32_t i) {
@@ -43,17 +50,7 @@ bool check_highest(uint32_t i) {
 	return pos + rightElems[i] >= subLen;
 }
 
-int main() {
-
-	scanf("%u %u %u ", &fullLen, &subLen, &carKinds);
-	for (uint32_t i = 0; i < fullLen; i++)
-		scanf("%u ", &full[i]);
-	for (uint32_t i = 0; i < subLen; i++)
-		scanf("%u ", &sub[i]);
-
-	/* *Elems - amount of prerequisites on left / right
-	 * having shortLen v shortLen-1 prerequisites doesn't change anything
-	 */
+void solve() {
 	{
 		uint32_t n = 0;
 		for (uint32_t i = 0; i < fullLen; i++) {
@@ -74,6 +71,7 @@ int main() {
 
 	{
 		static uint32_t posses[MAXAMT] = {0};
+		fill(begin(posses), end(posses), 0);
 		posses[sub[0]] = 1;
 		for (uint32_t i = 0; i < fullLen; i++) {
 			assert(full[i] < MAXAMT);
@@ -88,17 +86,53 @@ int main() {
 
 	{
 		for (uint32_t i = 0; i < fullLen; i++) {
-			bool a = check_naive(i);
 			bool b = check_highest(i);
+			bool a = check_naive(i);
 			// printf("%u %u %u  %u %u\n", full[i], leftElems[i], rightElems[i], a, b);
-			assert(a == b);
-			printf(b ? "1 " : "0 ");
+			if (a != b) {
+				puts("input:");
+				dump();
+				puts("highest:");
+				printarr(highest, fullLen);
+				assert(false);
+			}
+			// printf(b ? "1 " : "0 ");
 		}
-		puts("");
+		// puts("");
 	}
+}
 
-	// printarr(full, fullLen);
-	// printarr(leftElems, fullLen);
-	// printarr(rightElems, fullLen);
-	// printarr(result, fullLen);
+#include <cstdlib>
+
+int main() {
+	if (false) {
+		scanf("%u %u %u ", &fullLen, &subLen, &carKinds);
+		for (uint32_t i = 0; i < fullLen; i++)
+			scanf("%u ", &full[i]);
+		for (uint32_t i = 0; i < subLen; i++)
+			scanf("%u ", &sub[i]);
+
+		solve();
+	} else {
+		for (int i = 0; i < 10000; i++) {
+			if ((i & ((1 << 12) - 1)) == 0)
+				printf("%d\n", i);
+			srand(i);
+			fullLen = rand() % 10000 + 10;
+			if (rand() % 2) {
+				subLen = rand() % fullLen + 1;
+			} else {
+				subLen = 4;
+			}
+			carKinds = 1 << (rand() % 10);
+			assert(fullLen >= subLen);
+
+			for (uint32_t i = 0; i < fullLen; i++)
+				full[i] = rand() % carKinds + 1;
+			for (uint32_t i = 0; i < subLen; i++)
+				sub[i] = rand() % carKinds + 1;
+			//puts("");
+			solve();
+		}
+	}
 }
