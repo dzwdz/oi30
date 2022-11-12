af2fa07f401d192f37d79c3a22e4099f26911590
dzwdz <kg67199@gmail.com>
2022-11-12 20:42:57 +0100

    zbo: stuff

--- zbo/a2.py
+++ zbo/a2.py
@@ -18,7 +18,7 @@ princes = []
 for _ in range(k):
     princes.append(int(input()))
 
-def bfs_last(n, novisit=[]):
+def bfs_last(n, novisit=[]): # O(nodes) (if optimized further)
     assert len(nodes) > 2
     visited = defaultdict(lambda: False)
     frontier = deque([n])
@@ -33,7 +33,7 @@ def bfs_last(n, novisit=[]):
         if len(frontier) == 0:
             return a
 
-def bfs_intersect(n1, n2, novisit=[]):
+def bfs_intersect(n1, n2, novisit=[]): # O(nodes)
     if n1 == n2:
         return n2
     visited1 = defaultdict(lambda: False)
@@ -67,16 +67,14 @@ def dfs_dist(a, b, back=None):
             return r + weights[(a, e)]
     return None
 
-def dfs_isbetween(a, b, c, back=None, seen=False):
-    if a == b:
-        return seen
+def dirto(a, b):
+    if a == b: return a
     for e in edges[a]:
-        if e == back: continue
-        if dfs_isbetween(e, b, c, a, True if e == c else seen):
-            return True
-    return False
+        if dfs_dist(e, b, a) != None:
+            return e
+    assert False
 
-def tree_center(n, novisit=[]): # O(n)
+def tree_center(n, novisit=[]): # O(nodes)
     a = bfs_last(n, novisit)
     b = bfs_last(a, novisit)
     return bfs_intersect(a, b, novisit)
@@ -84,55 +82,58 @@ def tree_center(n, novisit=[]): # O(n)
 parents = defaultdict(lambda: None)
 parentdist = defaultdict(lambda: None)
 
-def tree_divide(root=None, novisit=[]): # probably O(n log n)ish
-    if not root:
-        root = tree_center(nodes[0]) # O(n)
+newroot = tree_center(nodes[0])
+toroot = defaultdict(lambda: None)
+for n in edges:
+    toroot[n] = dirto(n, newroot)
+
+def tree_divide(root=newroot, novisit=[]): # probably O(n log n)ish
     novisit = [root] + novisit
-    # max len(novisit) = log2(n) < 17
+    # max len(novisit) = log2(n) = log2(100000) < 17
 
     for n in edges[root]:
         # best: 2 iters, n halved
         # worst: n-1 iters, new n = 1
         if n in novisit: continue
-        c = tree_center(n, novisit) # overall: visits each node in the subtree once
+        a = bfs_last(n, novisit)
+        b = bfs_last(a, novisit)
+        c = bfs_intersect(a, b, novisit)
+
+        def dfs(a, b, last=None):
+            if a == b:
+                return (0, 0)
+            for e in edges[a]:
+                if e == last: continue
+                if e != root and e in novisit: continue
+                isback = toroot[a] == e
+                r = dfs(e, b, a)
+                if r:
+                    w = weights[(a, e)]
+                    return (r[0] + w, r[1] + (w if isback else 0))
+            return None
+
         parents[c] = root
-        parentdist[c] = dfs_dist(c, root)
+        parentdist[c] = dfs(root, c)
+        assert parentdist[c]
         # print(f"{c} came from {n}")
         tree_divide(c, novisit)
 
 tree_divide()
-# newroot = tree_center(nodes[0])
-
-def backs_away(n):
-    p = parents[n]
-    if not p: return False
-    p2 = parents[p]
-    if not p2: return False
-    return dfs_isbetween(p, p2, n)
 
 # (amt of connected villages, total distance)
 data = defaultdict(lambda: (0, 0))
 edgedata = defaultdict(lambda: (0,0)) # from (parent, child)
-def insert(n, data=data, edgedata=edgedata):
+def insert(n, data=data, edgedata=edgedata): # called k times
     r = data[n][1]
     d = 0 # distance from original n
     data[n] = (data[n][0] + 1, data[n][1])
     prev = None
     og_n = n
-    while True:
+    while True: # < log n   ?
         p = parents[n]
         if not p: break
 
-        # pd = parentdist
-        # if prev and backs_away(prev):
-        #     # d = d + parentdist[n] - parentdist[prev] * 2
-        #     # assert parentdist[n] > d
-        #     d = parentdist[n] - d
-        # else:
-        #     d += parentdist[n]
-        # print(f"{og_n} -> {p} = {d}")
-        d = dfs_dist(og_n, p)
-        assert dfs_dist(og_n, p) == d
+        d = dfs_dist(og_n, p) # O(?)
 
         data1 = data[p]
         data2 = edgedata[(p,n)]
@@ -143,21 +144,40 @@ def insert(n, data=data, edgedata=edgedata):
         n = p
     return r
 
-if False:
+import sys
+if sys.argv[1:] == ['graph']:
     print("digraph{")
     for k, v in parents.items():
         print(f"{k}->{v}[label=\"{parentdist[k]}\"]")
-    for n in edges:
-        if backs_away(n):
-            print(f"{n}[color=\"red\"]")
+        if parentdist[k][1] != 0:
+            print(f'{k}[color="red"]')
     print("}")
     exit()
+
+# if True:
+# def fastrootdist(n):
+#     p = parents[n]
+#     if not p:
+#         return 0 # we are root
+#     return dfs_dist(p, newroot) + parentdist[n][0] - 2 * parentdist[n][1]
+#     for n in edges:
+#         og = n
+#         assert dfs_dist(og, newroot) == fastrootdist(og)
+# 
+#         p = parents[n]
+#         if not p: continue
+#         d = parentdist[og][0]
+#         assert dfs_dist(og, p) == d
+# 
+#         n = p
+#         p = parents[n]
+#         if not p: continue
+#         print("og", 'n', 'p', 'goal', 'd', 'pd[n]', sep="\t")
+#         print(og, n, p, dfs_dist(og, p), d, parentdist[n], sep="\t")
+#         assert dfs_dist(og, p) == d + parentdist[n][0] - 2 * parentdist[og][1]
 
 total = 0
 insert(1)
 for p in princes:
     total += insert(p)
     print(total * 2)
-    # print("===", p, "\t", total * 2)
-    # for k, v in data.items():
-    #     print(k, v, sep="\t")
